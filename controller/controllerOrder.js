a084ebcf1da5a1fd386830b0d96f5049ffa1fdbb
vanhungn <vanhungnvh1712004@gmail.com>
2025-10-14 16:28:30 +0700

    ok

---
+++ controller/controllerOrder.js
@@ -0,0 +1,30 @@
+const modelOrder = require('../model/order')
+const CreateOrder = async (req, res) => {
+    try {
+        const order = req.body
+
+        if (!order || order.length < 0) {
+            return res.status(400).json({
+                message: "invite"
+            })
+        }
+        await modelOrder.insertMany(order.map(element => ({
+            idUser: element.idUser,
+            quantity: element.quantity,
+            color: element.color,
+            size: element.size,
+            img: element.img,
+            totalPrice: element.totalPrice,
+            status: "Chưa thanh toán",
+            address: element.address,
+            payment:element.payment,
+            name:element.name
+        })));
+        return res.status(200).json({
+            message: "Success"
+        })
+    } catch (error) {
+        return res.status(500).json({ error })
+    }
+}
+module.exports = { CreateOrder }
