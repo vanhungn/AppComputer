3e1c0364fd8b444512b3b801dc9e7e64e85b0838
vanhungn <vanhungnvh1712004@gmail.com>
2026-01-27 08:13:40 +0700

    ok

--- controller/controllerLogin.js
+++ controller/controllerLogin.js
@@ -29,7 +29,7 @@ const LoginGoogle = async (req, res) => {
         const refreshToken = await createToken({ email, name }, '1d', 'refreshToken')
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // chỉ true khi deploy
+            secure: true, // chỉ true khi deploy
             sameSite: 'strict',
             path: '/',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 ngày khớp với token
